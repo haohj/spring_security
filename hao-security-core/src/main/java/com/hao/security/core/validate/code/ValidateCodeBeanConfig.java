39ea41893c9da9f229c6a631a02cfce217d8870e
郝红剑 <hongjian.hao@zjtzsw.com>
2019-07-04 10:02:46 +0800

    spring security start

--- hao-security-core/src/main/java/com/hao/security/core/validate/code/ValidateCodeBeanConfig.java
+++ hao-security-core/src/main/java/com/hao/security/core/validate/code/ValidateCodeBeanConfig.java
@@ -2,16 +2,22 @@ package com.hao.security.core.validate.code;
 
 import com.hao.security.core.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ValidateCodeBeanConfig {
+    private final SecurityProperties securityProperties;
+
     @Autowired
-    private SecurityProperties securityProperties;
+    public ValidateCodeBeanConfig(SecurityProperties securityProperties) {
+        this.securityProperties = securityProperties;
+    }
 
     @Bean
-    public ValidateCodeGenerator validateCodeGenerator() {
+    @ConditionalOnMissingBean(name = "imageCodeGenerator")
+    public ValidateCodeGenerator imageCodeGenerator() {
         ImageCodeGenerator imageCodeGenerator = new ImageCodeGenerator();
         imageCodeGenerator.setSecurityProperties(securityProperties);
         return imageCodeGenerator;
